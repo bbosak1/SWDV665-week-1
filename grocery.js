a8abec0731150f779929b06191fd29982989c86c
bbosak1 <bbosak1@live.maryville.edu>
2020-07-03 13:30:36 -0500

    Add a grocery list and loop through to display

--- grocery.js
+++ grocery.js
@@ -5,8 +5,27 @@ var Grocery = /** @class */ (function () {
     }
     return Grocery;
 }());
-function createGroceries(grocery) {
-    return "Grocery + " + grocery.name;
+function createGroceries(groceryList) {
+    var returnStatement = 'My grocery list: ';
+    for (var _i = 0, groceryList_1 = groceryList; _i < groceryList_1.length; _i++) {
+        var element = groceryList_1[_i];
+        returnStatement = returnStatement + element.name + ". Quantity: " + element.quantity + " ";
+    }
+    return returnStatement;
 }
+var groceryList = [];
 var banana = new Grocery("banana", 3);
-document.body.textContent = createGroceries(banana);
+var orange = new Grocery("orange", 5);
+var grape = new Grocery("grape", 30);
+var kiwi = new Grocery("kiwi", 4);
+var canteloupe = new Grocery("canteloupe", 1);
+var watermelon = new Grocery("watermelon", 1);
+var strawberry = new Grocery("strawberry", 7);
+groceryList.push(banana);
+groceryList.push(orange);
+groceryList.push(grape);
+groceryList.push(kiwi);
+groceryList.push(canteloupe);
+groceryList.push(watermelon);
+groceryList.push(strawberry);
+document.body.textContent = createGroceries(groceryList);
--- grocery.ts
+++ grocery.ts
@@ -6,11 +6,28 @@ class Grocery {
   }
 }
 
-function createGroceries(grocery: Grocery) {
-  return "Grocery + " + grocery.name;
+function createGroceries(groceryList) {
+  let returnStatement = 'My grocery list: ';
+  for (let element of groceryList) {
+    returnStatement = returnStatement + element.name + ". Quantity: " + element.quantity + " ";
+  }
+  return returnStatement;
 }
 
+const groceryList = [];
 let banana = new Grocery("banana",3);
+let orange = new Grocery("orange",5);
+let grape = new Grocery("grape",30);
+let kiwi = new Grocery("kiwi",4);
+let canteloupe = new Grocery("canteloupe",1);
+let watermelon = new Grocery("watermelon",1);
+let strawberry = new Grocery("strawberry",7);
+groceryList.push(banana);
+groceryList.push(orange);
+groceryList.push(grape);
+groceryList.push(kiwi);
+groceryList.push(canteloupe);
+groceryList.push(watermelon);
+groceryList.push(strawberry);
 
-
-document.body.textContent = createGroceries(banana);
+document.body.textContent = createGroceries(groceryList);
